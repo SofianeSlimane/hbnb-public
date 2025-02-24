b0cc1a1e6c2fb5fc179d6e514b03cef24304f1a9
SofianeSlimane <sofianeslimane24@gmail.com>
2025-02-24 03:00:33 +0100

    change config

--- 03-frontend/mock-api/config.py
+++ 03-frontend/mock-api/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecretkey')
-    JWT_ACCESS_TOKEN_EXPIRES = False
+    JWT_ACCESS_TOKEN_EXPIRES = True
